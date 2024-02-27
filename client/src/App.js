66aeaa16878886aefe2d1bfcdfc282b6ccddead8
jeri <jeri@jeris-MacBook-Pro.local>
2024-02-27 13:47:03 -0500

    got data from the backend rendering in the UI

--- client/src/App.js
+++ client/src/App.js
@@ -8,19 +8,37 @@ function App() {
   //let [fieldValues, setFieldValues] = useState({ accessionID: '', sampleYear: '', sampleType: '' });
 
   // get ngs_samples from the db
-  let [NGSsamples, setNGSsamples] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get(`http://localhost:8080/api/get`)
-      .then(response => {
-        let data = response.data;
-        setNGSsamples({data});
-      })
-      .catch(function (error) { console.log(error); });
+    fetch(`http://localhost:8080/samples`)
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.log(err));
   }, [])
 
-  NGSsamples.map(function(item, i){
-    return <li>{item.sampleYear}</li>;
-  });
+  return data ?
+    <div style={{ padding: "50px" }}>
+      <div>You are looking at the frontend (client-side). Port 3000.</div>
+      <br></br>
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>AccessionID</th>
+          <th>sampleYear</th>
+          <th>sampleType</th>
+        </thead>
+        <tbody>
+          {data.map((d, i) => (
+            <tr key={i}>
+              <td>{d.id}</td>
+              <td>{d.accession_id}</td>
+              <td>{d.sample_year}</td>
+              <td>{d.sample_type}</td></tr>
+          ))}
+        </tbody>
+      </table>
+    </div> : null;
+
 }
 
 export default App;
